4111625ada2ddcc3a486077822f13a837cc64d3f
Sophie Picard <sogautrelet@gmail.com>
2023-10-05 14:19:37 +0200

    responsive on Error404 page

--- src/pages/Error404.js
+++ src/pages/Error404.js
@@ -6,7 +6,8 @@ const NotFound = () => {
     <div className="not-found">
       <h1 className="not-found__title">404</h1>
       <p className="not-found__text">
-        Oups! La page que vous demandez n'existe pas.
+        Oups! La page que{" "}
+        <span className="break">vous demandez n'existe pas.</span>
       </p>
       <Link to="/" className="not-found__link">
         Retourner sur la page d'accueil
